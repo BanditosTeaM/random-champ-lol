6720a10f8579ed52fff9499ac467bbc4d142fedc
BanditosTM <banditostm123@gmail.com>
2024-06-02 13:42:11 +0300

    chore: setup next config and init next-env

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ddragon.leagueoflegends.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "ddragon.canisback.com",
+        port: "",
+      },
+    ],
+  },
+};
+
+export default config;
