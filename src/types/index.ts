d989cc1df8d82e6f8795432692680c19231a0fdb
BanditosTM <banditostm123@gmail.com>
2024-06-03 22:15:23 +0300

    feat: adding export role

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export type { Champion } from "./Champion";
 export type { Item } from "./Item";
 export type { Rune } from "./Rune";
 export type { Summoner } from "./Summoner";
+export type { Role } from "./Role";
