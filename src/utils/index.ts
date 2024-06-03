4c210ac7dfc2355837a73616e340c121497fc4f1
BanditosTM <banditostm123@gmail.com>
2024-06-03 22:18:38 +0300

    feat: create file for fast import

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export { getRandomSummoners } from "./randomSummoners";
+export { getRandomRunes } from "./randomRunes";
+export { getRandomRole } from "./randomRoles";
+export { getRandomItems } from "./randomItems";
