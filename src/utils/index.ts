03baf856f3b9b4f3328a67dc464b0e7b1595b433
BanditosTM <banditostm123@gmail.com>
2024-06-04 14:50:40 +0300

    feat: adding export getRandomChampion

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+export { getRandomChampion } from "./randomChampion";
 export { getRandomSummoners } from "./randomSummoners";
 export { getRandomRunes } from "./randomRunes";
 export { getRandomRole } from "./randomRoles";
