bc13627ddf11137f33f0c60101dd1baf9622f215
BanditosTM <banditostm123@gmail.com>
2024-06-04 15:43:14 +0300

    feat: adding role selection

--- src/components/RandomRole/RandomRole.tsx
+++ src/components/RandomRole/RandomRole.tsx
@@ -1,13 +1,31 @@
 import { Role } from "@/types";
 import React from "react";
+import roleData from "@data/roles.json";
 
 interface RandomRoleProps {
   randomRole: Role[];
+  onChange: (name: string) => void;
+  excludedRoles: string[];
 }
 
-export const RandomRole = ({ randomRole }: RandomRoleProps) => {
+export const RandomRole = ({
+  randomRole,
+  onChange,
+  excludedRoles,
+}: RandomRoleProps) => {
   return (
     <section>
+      {roleData.map((role) => (
+        <label key={role.id}>
+          <input
+            type="checkbox"
+            checked={excludedRoles.includes(role.name)}
+            onChange={() => onChange(role.name)}
+          />
+          {role.name}
+        </label>
+      ))}
+
       <h1>Role</h1>
       {randomRole.map((role) => (
         <div key={role.id}>{role.name}</div>
