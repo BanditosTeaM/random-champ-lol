3dfd0ac2f1918e392473b49866f130a0a1098a98
BanditosTM <banditostm123@gmail.com>
2024-06-04 15:40:48 +0300

    feat: adding filter for selected roles

--- src/utils/randomRoles.ts
+++ src/utils/randomRoles.ts
@@ -1,11 +1,15 @@
 import roleData from "@/data/roles.json";
 import { Role } from "@/types";
 
-export const getRandomRole = (): Role[] => {
-  if (!roleData || roleData.length === 0) {
+export const getRandomRole = (excludedRoles: string[] = []): Role[] => {
+  const filteredRoles = roleData.filter(
+    (role) => !excludedRoles.includes(role.name)
+  );
+
+  if (filteredRoles.length === 0) {
     return [];
   }
 
-  const randomIndex = Math.floor(Math.random() * roleData.length);
-  return [roleData[randomIndex]];
+  const randomIndex = Math.floor(Math.random() * filteredRoles.length);
+  return [filteredRoles[randomIndex]];
 };
