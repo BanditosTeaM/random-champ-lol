7a82b8e4f1072c2fa38dd318f6cb5d5ab132cafb
BanditosTM <banditostm123@gmail.com>
2024-06-04 13:31:28 +0300

    feat: adding role check in function getRandomSummoners

--- src/utils/randomSummoners.ts
+++ src/utils/randomSummoners.ts
@@ -15,19 +15,35 @@ const formattedSummoners = filteredSummoners.map(([, item]) => ({
   height: item.image.h,
 }));
 
-export const getRandomSummoners = () => {
+export const getRandomSummoners = (role: string) => {
   if (formattedSummoners.length === 0) return;
 
   const selectedItems = [...formattedSummoners];
   const finalSelectedItems: Summoner[] = [];
+  const checkItems = new Set();
 
-  for (let i = 0; i < 2; i++) {
+  do {
     const randomIndex = Math.floor(Math.random() * selectedItems.length);
     const selectedItem = selectedItems[randomIndex];
     selectedItems.splice(randomIndex, 1);
 
-    finalSelectedItems.push(selectedItem);
-  }
+    if (role === "Jungle") {
+      const smiteItem = formattedSummoners.find(
+        (summoner) => summoner.name === "Smite"
+      );
+
+      if (smiteItem && !checkItems.has(smiteItem.name)) {
+        finalSelectedItems.push(smiteItem);
+        checkItems.add(smiteItem.name);
+        continue;
+      }
+    }
+
+    if (!checkItems.has(selectedItem.name)) {
+      finalSelectedItems.push(selectedItem);
+      checkItems.add(selectedItem.name);
+    }
+  } while (finalSelectedItems.length < 2);
 
   return finalSelectedItems;
 };
