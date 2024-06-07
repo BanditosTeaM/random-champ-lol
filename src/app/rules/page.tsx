15577790dc998f346816c458e0e3f592cda4aa3b
BanditosTM <banditostm123@gmail.com>
2024-06-07 17:15:08 +0300

    feat: adding rules

---
+++ src/app/rules/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+export default function Page() {
+  return (
+    <>
+      <section>
+        <h1>Rules</h1>
+        <section>
+          <h2>1. No Turning Back</h2>
+          <p>
+            There is no return. Once you got your item build, stick with it!
+          </p>
+        </section>
+        <section>
+          <h2>2. Buy items in the right order</h2>
+          <p>The items have to be bought in the correct order.</p>
+        </section>
+        <section>
+          <h2>3. Never surrender</h2>
+          <p>Fight for the last tower. There is no surrendering in the rift.</p>
+        </section>
+        <section>
+          <h2>4. Don't use wards</h2>
+          <p>You must not buy wards.</p>
+        </section>
+        <section>
+          <h2>5. Use the given summoners spells</h2>
+          <p>
+            Take the summoner spells provided by your build and you use them as
+            much as possible.
+          </p>
+        </section>
+        <section>
+          <h2>6. Role-specific item</h2>
+          <p>
+            You can buy the role-specific item first, but you have to sell it to
+            complete your full build.
+          </p>
+        </section>
+      </section>
+    </>
+  );
+}
