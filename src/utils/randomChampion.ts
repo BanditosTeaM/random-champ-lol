bea002ff71b526a2599643265e314d3f9b1e11f3
BanditosTM <banditostm123@gmail.com>
2024-06-07 17:20:08 +0300

    feat: adding filter for champions

--- src/utils/randomChampion.ts
+++ src/utils/randomChampion.ts
@@ -1,15 +1,20 @@
 import championsData from "@data/champion.json";
 
-export const getRandomChampion = () => {
+export const getRandomChampion = (excludedChampions: string[]) => {
   const championEntries = Object.values(championsData.data);
+
+  const filteredChampions = championEntries.filter((champion) =>
+    excludedChampions.includes(champion.name)
+  );
 
   const randomItem = [];
 
   for (let index = 0; index < 30; index++) {
-    const randomIndex = Math.floor(Math.random() * championEntries.length);
-    const champion = championEntries[randomIndex];
+    const randomIndex = Math.floor(Math.random() * filteredChampions.length);
+    const champion = filteredChampions[randomIndex];
+
     const selectedChampion = {
-      version: champion.version,
+      version: championsData.version,
       name: champion.name,
       image: champion.image.full,
       width: champion.image.w,
