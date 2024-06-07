5da71db27904d7b573a285ab7f5e39ed283299b1
BanditosTM <banditostm123@gmail.com>
2024-06-07 17:15:57 +0300

    feat: import header and adding styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
+import { Header } from "@/components/Header";
+import styles from "./styles.module.css";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={styles.html}>
+      <body className={styles.body}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
