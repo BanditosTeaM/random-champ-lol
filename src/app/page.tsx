62e8b171c9e9e8e4442817ab1f94a3e866228d0f
BanditosTM <banditostm123@gmail.com>
2024-06-07 17:18:36 +0300

    refactor: remove link to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
 export default function Page() {
   return (
     <>
       <div>Hello, its first page</div>
-      <Link href="/dashboard">Random build</Link>
     </>
   );
 }
